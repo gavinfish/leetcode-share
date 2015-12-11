17fefac83665c08db82874d9ef0ecacb17aebc85
Jie Shen <1993sj1993@gmail.com>
2015-12-11 16:14:45 +0800

    Two Sum/Add Two Numbers

---
+++ 1 Two Sum.py
@@ -0,0 +1,30 @@
+'''
+Given an array of integers, find two numbers such that they add up to a specific target number.
+
+The function twoSum should return indices of the two numbers such that they add up to the target, where index1 must be less than index2. Please note that your returned answers (both index1 and index2) are not zero-based.
+
+You may assume that each input would have exactly one solution.
+
+Input: numbers={2, 7, 11, 15}, target=9
+Output: index1=1, index2=2
+'''
+
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        hash_map = {}
+        for index, value in enumerate(nums):
+            hash_map[value] = index
+        for index1, value in enumerate(nums):
+            if target - value in hash_map:
+                index2 = hash_map[target - value]
+                if index1 != index2:
+                    return [index1 + 1, index2 + 1]
+
+
+if __name__ == "__main__":
+    print(Solution().twoSum([2, 7, 11, 15], 9));
---
+++ 2 Add Two Numbers.py
@@ -0,0 +1,59 @@
+'''
+You are given two linked lists representing two non-negative numbers. The digits are stored in reverse order and each of their nodes contain a single digit. Add the two numbers and return it as a linked list.
+
+Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+Output: 7 -> 0 -> 8
+'''
+
+# Definition for singly-linked list.
+class ListNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+    # Define this to check if it works well
+    def myPrint(self):
+        print(self.val)
+        if self.next:
+            self.next.a()
+
+
+class Solution(object):
+    def addTwoNumbers(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        result = ListNode(0);
+        cur = result;
+        while l1 or l2:
+            cur.val += self.addTwoNodes(l1, l2)
+            if cur.val >= 10:
+                cur.val -= 10
+                cur.next = ListNode(1)
+            else:
+                if l1 and l1.next or l2 and l2.next:
+                    cur.next = ListNode(0)
+            cur = cur.next
+            if l1:
+                l1 = l1.next
+            if l2:
+                l2 = l2.next
+        return result
+
+    def addTwoNodes(self, n1, n2):
+        if not n1 and not n2:
+            # This cannot happen, ignore it
+            None
+        if not n1:
+            return n2.val
+        if not n2:
+            return n1.val
+        return n1.val + n2.val
+
+
+if __name__ == "__main__":
+    list = ListNode(9)
+    list.next = ListNode(8)
+    print(Solution().addTwoNumbers(list, ListNode(1)).myPrint())
