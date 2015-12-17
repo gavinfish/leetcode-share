4c8c636c90ca7a0fa89520b4ef4a51af93d118e6
DRFish <1993sj1993@gmail.com>
2015-12-17 20:49:32 +0800

    Add dp solution.

--- 10 Regular Expression Matching.py
+++ 10 Regular Expression Matching.py
@@ -21,7 +21,7 @@ isMatch("aab", "c*a*b") → true
 
 class Solution(object):
     # Time Limit Exceeded!!!
-    def isMatch(self, s, p):
+    def isMatch_timeout(self, s, p):
         """
         :type s: str
         :type p: str
@@ -36,7 +36,7 @@ class Solution(object):
                 return self.isMatch(s, p[2:])
             else:
                 return False
-        # When the the second char of pattern is "*"
+        # When the the second character of pattern is "*"
         if 1 < len(p) and p[1] == "*":
             # When the first character matches, there are three possible situation
             if s[0] == p[0] or p[0] == ".":
@@ -53,22 +53,21 @@ class Solution(object):
                 return False
 
 
+class Solution(object):
     def isMatch(self, s, p):
         """
-        define dp[i][j] = tape[i:] and regex[j:] are matched
-        :param s:
-        :param p:
-        :return: boolean
+        :type s: str
+        :type p: str
+        :rtype: bool
         """
-
         m = len(s)
         n = len(p)
         # Init dp
-        dp = [[False for i in xrange(n + 1)] for i in xrange(m + 1)]
+        dp = [[False for i in range(n + 1)] for i in range(m + 1)]
         # When string and pattern are all None
         dp[m][n] = True
-        #
-        for i in xrange(n - 1, -1, -1):
+        # When the string is None, pattern like "a*" can still match it
+        for i in range(n - 1, -1, -1):
             if p[i] == "*":
                 dp[m][i] = dp[m][i + 1]
             elif i + 1 < n and p[i + 1] == "*":
@@ -76,26 +75,33 @@ class Solution(object):
             else:
                 dp[m][i] = False
 
-
-        for i in xrange(m - 1, -1, -1):
-            for j in xrange(n - 1, -1, -1):
+        for i in range(m - 1, -1, -1):
+            for j in range(n - 1, -1, -1):
+                # When the current character is "*"
                 if p[j] == "*":
                     if j - 1 >= 0 and p[j - 1] != "*":
                         dp[i][j] = dp[i][j + 1]
+                    # If the pattern is starting with "*" or has "**" in it
                     else:
-                        return False 
+                        return False
+                # When the the second character of pattern is "*"
                 elif j + 1 < n and p[j + 1] == "*":
+                    # When the current character matches, there are three possible situation
+                    # 1. ".*" matches nothing
+                    # 2. "c*" matches more than one character
+                    # 3. "c*" just matches one character
                     if s[i] == p[j] or p[j] == ".":
                         dp[i][j] = dp[i][j + 2] or dp[i + 1][j] or dp[i + 1][j + 2]
+                    # Ignore the first two characters("c*") in pattern since they cannot match
+                    # the current character in string
                     else:
                         dp[i][j] = dp[i][j + 2]
                 else:
+                    # When the current character is matched
                     if s[i] == p[j] or p[j] == ".":
                         dp[i][j] = dp[i + 1][j + 1]
                     else:
                         dp[i][j] = False
-
-
         return dp[0][0]
 
 
