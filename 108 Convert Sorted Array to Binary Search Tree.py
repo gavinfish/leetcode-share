8260140a5016ab6bd0fcb2f6b55f01e15b90cfdf
Jie Shen <1993sj1993@gmail.com>
2016-02-18 17:11:39 +0800

    Convert Sorted Array to Binary Search Tree/ Convert Sorted List to Binary Search Tree

---
+++ 108 Convert Sorted Array to Binary Search Tree.py
@@ -0,0 +1,28 @@
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution(object):
+    def sortedArrayToBST(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: TreeNode
+        """
+        return self._sortedArrayToBST(nums, 0, len(nums))
+
+    def _sortedArrayToBST(self, nums, left, right):
+        if left == right:
+            return None
+        mid = (left + right) >> 1
+        root = TreeNode(nums[mid])
+        root.left = self._sortedArrayToBST(nums, left, mid)
+        root.right = self._sortedArrayToBST(nums, mid + 1, right)
+        return root
+
+
+if __name__ == "__main__":
+    None
---
+++ 109 Convert Sorted List to Binary Search Tree.py
@@ -0,0 +1,42 @@
+# Definition for singly-linked list.
+class ListNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution(object):
+    def sortedListToBST(self, head):
+        """
+        :type head: ListNode
+        :rtype: TreeNode
+        """
+        node, length = head, 0
+        while node:
+            node = node.next
+            length += 1
+        self.curr = head
+        return self._sortedListToBST(0, length - 1)
+
+    def _sortedListToBST(self, left, right):
+        if left > right:
+            return None
+        mid = (left + right) // 2
+        left = self._sortedListToBST(left, mid - 1)
+        root = TreeNode(self.curr.val)
+        root.left = left
+        self.curr = self.curr.next
+        root.right = self._sortedListToBST(mid + 1, right)
+        return root
+
+
+if __name__ == "__main__":
+    None
