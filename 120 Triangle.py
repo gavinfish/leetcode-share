ecd22c1d6a2b2602ebaeff7df31c4118360c54ac
DRFish <1993sj1993@gmail.com>
2016-03-05 16:48:26 +0800

    Triangle/ Best Time to Buy and Sell Stock

---
+++ 120 Triangle.py
@@ -0,0 +1,37 @@
+'''
+Given a triangle, find the minimum path sum from top to bottom. Each step you may move to adjacent numbers on the row below.
+
+For example, given the following triangle
+[
+     [2],
+    [3,4],
+   [6,5,7],
+  [4,1,8,3]
+]
+The minimum path sum from top to bottom is 11 (i.e., 2 + 3 + 5 + 1 = 11).
+
+Note:
+Bonus point if you are able to do this using only O(n) extra space, where n is the total number of rows in the triangle.
+'''
+
+class Solution(object):
+    def minimumTotal(self, triangle):
+        """
+        :type triangle: List[List[int]]
+        :rtype: int
+        """
+        n = len(triangle)
+        dp = triangle[-1]
+        for i in range(n - 2, -1, -1):
+            for j in range(i + 1):
+                dp[j] = triangle[i][j] + min(dp[j], dp[j + 1])
+        return dp[0]
+
+
+if __name__ == "__main__":
+    assert Solution().minimumTotal([
+        [2],
+        [3, 4],
+        [6, 5, 7],
+        [4, 1, 8, 3]
+    ]) == 11
---
+++ 121 Best Time to Buy and Sell Stock.py
@@ -0,0 +1,28 @@
+'''
+Say you have an array for which the ith element is the price of a given stock on day i.
+
+If you were only permitted to complete at most one transaction (ie, buy one and sell one share of the stock), design an algorithm to find the maximum profit.
+'''
+
+class Solution(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        if len(prices) < 2:
+            return 0
+        max_price = min_price = prices[0]
+        max_profit = 0
+        for price in prices:
+            if price > max_price:
+                max_price = price
+            if price < min_price:
+                max_price = min_price = price
+            if max_price - min_price > max_profit:
+                max_profit = max_price - min_price
+        return max_profit
+
+
+if __name__ == "__main__":
+    assert Solution().maxProfit([2, 4, 6, 1, 3, 8, 3]) == 7
