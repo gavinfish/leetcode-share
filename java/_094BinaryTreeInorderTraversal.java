ef34e89fe38e29b00fc9f4d36420f7dde6be5d8b
Shen Jie <drfish.me@gmail.com>
2017-05-27 22:58:29 +0800

    Java solution 94 && 96

---
+++ java/_094BinaryTreeInorderTraversal.java
@@ -0,0 +1,49 @@
+/**
+ * Given a binary tree, return the inorder traversal of its nodes' values.
+ * <p>
+ * For example:
+ * Given binary tree [1,null,2,3],
+ *  1
+ *   \
+ *    2
+ *  /
+ * 3
+ * return [1,3,2].
+ * <p>
+ * Note: Recursive solution is trivial, could you do it iteratively?
+ * Created by drfish on 27/05/2017.
+ */
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+/**
+ * Definition for a binary tree node.
+ */
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
+
+public class _094BinaryTreeInorderTraversal {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        Stack<TreeNode> stack = new Stack<>();
+        List<Integer> result = new ArrayList<>();
+        while (root != null || !stack.empty()) {
+            while (root != null) {
+                stack.add(root);
+                root = root.left;
+            }
+            root = stack.pop();
+            result.add(root.val);
+            root = root.right;
+        }
+        return result;
+    }
+}
---
+++ java/_096UniqueBinarySearchTrees.java
@@ -0,0 +1,34 @@
+/**
+ * Given n, how many structurally unique BST's (binary search trees) that store values 1...n?
+ * <p>
+ * For example,
+ * Given n = 3, there are a total of 5 unique BST's.
+ * <p>
+ * 1         3     3      2      1
+ *  \       /     /      / \      \
+ *   3     2     1      1   3      2
+ *  /     /       \                 \
+ * 2     1         2                 3
+ * <p>
+ * Created by drfish on 27/05/2017.
+ */
+public class _096UniqueBinarySearchTrees {
+    public int numTrees(int n) {
+        int[] counts = new int[n + 1];
+        counts[0] = 1;
+        for (int i = 1; i <= n; i++) {
+            int count = 0;
+            for (int j = 0; j < i; j++) {
+                count += counts[j] * counts[i - 1 - j];
+            }
+            counts[i] = count;
+        }
+        return counts[n];
+    }
+
+    public static void main(String[] args) {
+        _096UniqueBinarySearchTrees uniqueBinarySearchTrees = new _096UniqueBinarySearchTrees();
+        System.out.println(uniqueBinarySearchTrees.numTrees(6));
+        assert uniqueBinarySearchTrees.numTrees(3) == 5;
+    }
+}
