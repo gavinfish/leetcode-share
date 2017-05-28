360bec9ca6872979d814da6571c3dd2ab64a8f2a
Shen Jie <drfish.me@gmail.com>
2017-05-28 10:27:40 +0800

    Add description for java 95

--- java/_095UniqueBinarySearchTreesII.java
+++ java/_095UniqueBinarySearchTreesII.java
@@ -2,6 +2,17 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
+ * Given an integer n, generate all structurally unique BST's (binary search trees) that store values 1...n.
+ * <p>
+ * For example,
+ * Given n = 3, your program should return all 5 unique BST's shown below.
+ * <p>
+ * 1         3     3      2      1
+ *  \       /     /      / \      \
+ *   3     2     1      1   3      2
+ *  /     /       \                 \
+ * 2     1         2                 3
+ * <p>
  * Created by drfish on 27/05/2017.
  */
 public class _095UniqueBinarySearchTreesII {
