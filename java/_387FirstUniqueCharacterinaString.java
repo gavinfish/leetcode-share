2a71961be8f2cc3aec0365d1fc6c4a152a99d46c
drfish <drfish.me@gmail.com>
2017-11-30 20:01:00 +0800

    Java solution 387

---
+++ java/_387FirstUniqueCharacterinaString.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ * Given a string, find the first non-repeating character in it and return it's index. If it doesn't exist, return -1.
+ * 
+ * Examples:
+ * 
+ * s = "leetcode"
+ * return 0.
+ * 
+ * s = "loveleetcode",
+ * return 2.
+ * Note: You may assume the string contain only lowercase letters.
+ */
+public class _387FirstUniqueCharacterinaString {
+    public int firstUniqChar(String s) {
+        int[] frequency = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            frequency[s.charAt(i) - 'a']++;
+        }
+        for (int i = 0; i < s.length(); i++) {
+            if (frequency[s.charAt(i) - 'a'] == 1) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
